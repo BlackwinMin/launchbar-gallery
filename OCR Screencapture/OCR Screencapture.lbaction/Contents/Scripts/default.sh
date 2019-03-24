0129e6d1592d7bbf5e407e088495eb1cf4ac96a2
Min <Min@MacBook.local>
2019-03-24 15:03:38 +0800

    polish code | by Minja from MacBook

--- OCR Screencapture/OCR Screencapture.lbaction/Contents/Scripts/default.sh
+++ OCR Screencapture/OCR Screencapture.lbaction/Contents/Scripts/default.sh
@@ -3,5 +3,6 @@
 
 PATH=$PATH:/usr/local/bin/
 screencapture -i ~/ocr.png
-tesseract ~/ocr.png stdout -l chi_sim+eng | pbcopy
+#tesseract ~/ocr.png stdout -l chi_sim+eng | pbcopy
+tesseract ~/ocr.png stdout --oem 1 --psm 6 -l chi_sim+eng -c preserve_interword_spaces=1 | pbcopy
 rm ~/ocr.png
