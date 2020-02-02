4cd67b7390a665facb45485b90752441fa8d645d
Minja test <blackwinmin@outlook.com>
2020-02-03 00:51:10 +0800

    img Search | by Minja from MacBook

--- img Search/img Search.lbaction/Contents/Scripts/default.sh
+++ img Search/img Search.lbaction/Contents/Scripts/default.sh
@@ -7,7 +7,7 @@
 
 echo "$# arguments passed"
 for ARG in "$@"; do
-    var=$(curl -F smfile=@"$@" https://sm.ms/api/upload | grep -o -E 'https:\\/\\/i\.loli\.net\\.*\.png|https:\\/\\/i\.loli\.net\\.*\.jpg|https:\\/\\/i\.loli\.net\\.*\.jpeg' | sed 's/\\//g')
+    var=$(curl -F smfile=@"$@" https://sm.ms/api/v2/upload | grep -o -E 'https:\\/\\/i\.loli\.net\\.*\.png|https:\\/\\/i\.loli\.net\\.*\.jpg|https:\\/\\/i\.loli\.net\\.*\.jpeg' | sed 's/\\//g')
     var2=https://images.google.com/searchbyimage?image_url=$var
     open -a Safari $var2
 done
