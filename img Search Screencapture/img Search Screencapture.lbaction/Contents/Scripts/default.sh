16a05478be57c8f1444f45814cc7d614d5f3984f
Minja test <blackwinmin@outlook.com>
2020-02-03 22:51:13 +0800

    update action | by Minja from MacBook

--- img Search Screencapture/img Search Screencapture.lbaction/Contents/Scripts/default.sh
+++ img Search Screencapture/img Search Screencapture.lbaction/Contents/Scripts/default.sh
@@ -1,10 +1,11 @@
 # LaunchBar Action Script
 #
-# 作者：Minja
+# 作者：Minja, dofy
 #
 
 screencapture -i ~/googleimagesearch.png
-var=$(curl -F smfile=@"/Users/apple/googleimagesearch.png" https://sm.ms/api/v2/upload | grep -o -E 'https:\\/\\/i\.loli\.net\\.*\.png|https:\\/\\/i\.loli\.net\\.*\.jpg|https:\\/\\/i\.loli\.net\\.*\.jpeg' | sed 's/\\//g')
+#var=$(curl -F smfile=@"/Users/apple/googleimagesearch.png" https://sm.ms/api/v2/upload | grep -o -E 'https:\\/\\/i\.loli\.net\\.*\.png|https:\\/\\/i\.loli\.net\\.*\.jpg|https:\\/\\/i\.loli\.net\\.*\.jpeg' | sed 's/\\//g')
+var=$(curl -F smfile=@"/Users/apple/googleimagesearch.png" https://sm.ms/api/v2/upload | grep -o -E '"https:\\/\\/i\.loli\.net\\.*\.(png|jpg|jpeg)"' | sed 's/\\//g' | sed 's/"//g')
 var2=https://images.google.com/searchbyimage?image_url=$var
 open -a Safari $var2
 rm ~/googleimagesearch.png
--- img Search/img Search.lbaction/Contents/Scripts/default.sh
+++ img Search/img Search.lbaction/Contents/Scripts/default.sh
@@ -2,12 +2,13 @@
 #
 # LaunchBar Action Script
 #
-# 作者：Minja
+# 作者：Minja, dofy
 #
 
 echo "$# arguments passed"
 for ARG in "$@"; do
-    var=$(curl -F smfile=@"$@" https://sm.ms/api/v2/upload | grep -o -E 'https:\\/\\/i\.loli\.net\\.*\.png|https:\\/\\/i\.loli\.net\\.*\.jpg|https:\\/\\/i\.loli\.net\\.*\.jpeg' | sed 's/\\//g')
+#    var=$(curl -F smfile=@"$@" https://sm.ms/api/v2/upload | grep -o -E 'https:\\/\\/i\.loli\.net\\.*\.png|https:\\/\\/i\.loli\.net\\.*\.jpg|https:\\/\\/i\.loli\.net\\.*\.jpeg' | sed 's/\\//g')
+    var=$(curl -X POST -F smfile=@"$ARG" https://sm.ms/api/v2/upload | grep -o -E '"https:\\/\\/i\.loli\.net\\.*\.(png|jpg|jpeg)"' | sed 's/\\//g' | sed 's/"//g')
     var2=https://images.google.com/searchbyimage?image_url=$var
     open -a Safari $var2
 done
