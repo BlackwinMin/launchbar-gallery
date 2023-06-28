933f8fd788938aa852085387fe70e9e301751a46
Minja test <blackwinmin@outlook.com>
2023-06-28 20:09:24 +0800

    new: Video Add Cover | by Minja from MacBook

---
+++ Video Add Cover/README.md
@@ -0,0 +1,9 @@
+# Video Add Cover 为视频文件添加封面
+
+借助 FFmpeg，选中视频文件和图片素材，即可将图片添加为视频文件的封面。
+
+出处：[《如何为视频快速添加封面（附 Shortcuts 及其他动作下载）》](https://utgd.net)（发布时间未定）。
+
+另有 [Shortcuts 版](https://github.com/BlackwinMin/Shortcuts-Actions-gallery/tree/master/Video%20Add%20Cover)。
+
+![title](img.png)
---
+++ Video Add Cover/Video add Cover.lbaction/Contents/Scripts/default.sh
@@ -0,0 +1,17 @@
+# LaunchBar Action Script
+#
+
+PATH=$PATH:/usr/local/bin/
+
+for f in "$@";do
+
+if [[ $f == *.jpg ]] || [[ $f == *.JPG ]]; then
+        imageFile="$f"
+elif [[ $f == *.mp4 ]]; then
+        videoFile="$f"
+fi
+
+ffmpeg -i "$videoFile" -i "$imageFile" -map 0 -map 1 -c copy -disposition:v:1 attached_pic "${imageFile%.*}-2.mp4"
+
+#    ffmpeg -i "$f" -map 0:v -map -0:V -c copy "$newFile"
+done && afplay "/System/Library/Sounds/Submarine.aiff" && osascript -e "tell application \"LaunchBar\" to display in notification center \"Create Cover for ${imageFile%.*}\" with title \"Video to Cover\""
